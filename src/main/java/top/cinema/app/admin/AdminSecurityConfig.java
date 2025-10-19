323c859161cf8719dc4b1236f9a75a2fd232edf8
Bartosz Dobija <33185107+BartShoot@users.noreply.github.com>
2025-10-19 07:16:30 +0200

    forwarding headers support

--- src/main/java/top/cinema/app/admin/AdminSecurityConfig.java
+++ src/main/java/top/cinema/app/admin/AdminSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 import top.cinema.app.user.JpaUserDetailsService;
 
 import javax.sql.DataSource;
@@ -68,5 +69,10 @@ public class AdminSecurityConfig {
         JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
         tokenRepository.setDataSource(dataSource);
         return tokenRepository;
+    }
+
+    @Bean
+    public ForwardedHeaderFilter forwardedHeaderFilter() {
+        return new ForwardedHeaderFilter();
     }
 }
